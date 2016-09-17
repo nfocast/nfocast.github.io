6eddbbd6d9fae683c25b2662f617ce1026366125
nfocast <nfocast@gmail.com>
2016-09-17 14:40:55 -0400

    Update tipuedrop_content.js

--- js/tipuedrop_content.js
+++ js/tipuedrop_content.js
@@ -1,5 +1,5 @@
 
-var tipuesearch = {"pages": [
+var tipuesearch = {" {{ site.pages }}": [
      {"title": "Tipue", "text": "", "tags": "jQuery HTML5 CSS", "url": "http://nfocast.github.io"},
     
     
