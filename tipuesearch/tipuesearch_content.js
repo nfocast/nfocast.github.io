ff5bbda662d289c28bedb8ab5839c39b92ecb62a
nfocast <nfocast@gmail.com>
2016-09-22 23:16:19 -0400

    Update tipuesearch_content.js

--- tipuesearch/tipuesearch_content.js
+++ tipuesearch/tipuesearch_content.js
@@ -1,10 +1,16 @@
-
-var tipuesearch = {"pages": [
+---
+content_type: json
+layout: none
+posts_per_page: 9999
+single_page: true
+format: none
+posts_filters:
+    not:
+        has_tags: draft
+---
+{"pages": [
     {% for post in pagination.posts %}
-    {% include 'Tipue-Search.jquery.json' %}
+    {% include 'searchdata.json' %}
     {% endfor %}
     {"title": "", "text": "", "tags": "", "loc": ""}
-]};
-
-
-
+]}
