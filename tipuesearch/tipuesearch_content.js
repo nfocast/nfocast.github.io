b476fd77fa37b034ed45ede1cee2a68b3bc380dd
nfocast <nfocast@gmail.com>
2016-09-22 23:33:46 -0400

    Update tipuesearch_content.js

--- tipuesearch/tipuesearch_content.js
+++ tipuesearch/tipuesearch_content.js
@@ -1,6 +1,6 @@
 var tipuesearch = {"pages": [
  {"title": "posts", "text": "", "tags": "jQuery HTML5 CSS", "url": nfocast.github.io"},
- {"url": href="nfocast.github.io/posts/"},
+ {"url": href="nfocast.github.io/posts/"}
 
 
 ]};
